33b38f0a3990de5282b5f9d40e6b07dda874d9a9
karan kartikeya <karankartikey72@gmail.com>
2022-09-24 19:04:01 +0530

    feat: navbar added

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -3,6 +3,7 @@ import {Cursor, useTypewriter} from "react-simple-typewriter";
 import BackgroundCircles from './BackgroundCircles';
 import image from 'next/image';
 import Imagedata from './Imageback';
+import Link from 'next/link';
 
 
 
@@ -27,10 +28,33 @@ export default function Hero({}: Props) {
     <div className='h-screen flex flex-col space-y-8 items-center justify-center text-center overflow-hidden'>
         <BackgroundCircles/>
         <Imagedata/>
-        <h1>
-            {text}
-            <Cursor cursorColor='yellow'></Cursor>
-        </h1>
+        <div className='z-20'>
+            <h1 className='text-sm uppercase text-gray-500 pb-2 tracking-[10px]'>SOftware Engineer</h1>
+            <h1 className='text-5xl lg:text-6xl font-semibold scroll-px-10'>
+                <span className='mr-3'>{text}</span>
+                <Cursor cursorColor='yellow'></Cursor>
+            </h1>
+            <div className='mt-4'>
+                <Link href='#about'>
+                <button className='heroButton'>About</button>
+                </Link>
+
+                <Link href='#experience'>
+                <button className='heroButton'>Experience</button>
+                </Link>
+
+                <Link href='#projects'>
+                <button className='heroButton'>Projects</button>
+                </Link>
+
+                <Link href='#skills'>
+                <button className='heroButton'>Skills</button>
+                </Link>
+
+               
+
+            </div>
+        </div>
     </div>
   )
 }
