ad12094578c01cd766d6a181982567ccf4b4246b
karan kartikeya <karankartikey72@gmail.com>
2022-09-24 18:35:43 +0530

    fix: rounded the images corner

--- components/Imageback.tsx
+++ components/Imageback.tsx
@@ -6,16 +6,17 @@ type Props = {}
 export default function Imagedata({}: Props) {
 
     const myLoader = ({ src, width, quality }) => {
-        return `https://images.unsplash.com/photo-1662990502014-9b73af9383d9?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=720&q=80`
+        return `https://karankartikeya.netlify.app/images/kk7.jpg`
       }
     
   return (
     <Image
+    className='relative rounded-full h-32 w-32 mx-auto'
         loader={myLoader}
         src=" "
         alt = "picture of hero"
-        width={100}
-        height={100}
+        width={150}
+        height={150}
     />
   )
 }
