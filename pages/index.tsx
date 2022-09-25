3e3bbb5cede932d58df47e7ef5631edfdf727a6a
karan kartikeya <karankartikey72@gmail.com>
2022-09-25 20:41:33 +0530

    added experience section

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,7 @@ import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
 import About from '../components/About'
+import Experience from '../components/Experience'
 import Header from '../components/Header'
 import Hero from '../components/Hero'
 
@@ -26,8 +27,10 @@ const Home: NextPage = () => {
         <About/>
       </section>
 
-      {/* Header */}
-
+      {/* Experience */}
+      <section id='experience' className='snap-center'>
+        <Experience/>
+      </section>
       {/* Header */}
 
       {/* Skills */}
