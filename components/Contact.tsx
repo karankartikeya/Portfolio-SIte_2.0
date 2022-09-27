35b9db8c0cc37d8379455ef6a985f18529e54302
karan kartikeya <karankartikey72@gmail.com>
2022-09-27 20:07:38 +0530

    fix: implemented heroicons

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-
+import {PhoneIcon, MapPinIcon, EnvelopeIcon} from '@heroicons/react/24/solid'
 type Props = {}
 
 function Contact({}: Props) {
@@ -15,8 +15,19 @@ function Contact({}: Props) {
                 I have got what you want bro!!!
             </h4>
 
-            <div>
-                
+            <div className='flex items-center space-x-5 justify-center'>
+                <PhoneIcon className='animate-pulse h-7 w-7 text-yellow-400'/>
+                <p className='text-2xl'>+917409130005</p>
+            </div>
+
+            <div className='flex items-center space-x-5 justify-center'>
+                <MapPinIcon className='animate-pulse h-7 w-7 text-yellow-400'/>
+                <p className='text-2xl'>123 developer</p>
+            </div>
+
+            <div className='flex items-center space-x-5 justify-center'>
+                <EnvelopeIcon className='animate-pulse h-7 w-7 text-yellow-400'/>
+                <p className='text-2xl' ><a href='mailto:karankartikey72@gmail.com'>Karankartikey72@gmail.com</a></p>
             </div>
         </div>
     </div>
