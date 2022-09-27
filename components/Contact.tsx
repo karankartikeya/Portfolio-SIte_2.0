efa5b41648060638cbaadf301bffa7c87363e365
karan kartikeya <karankartikey72@gmail.com>
2022-09-27 21:18:01 +0530

    added react-hook-form

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -1,9 +1,30 @@
 import React from 'react'
 import {PhoneIcon, MapPinIcon, EnvelopeIcon} from '@heroicons/react/24/solid'
+import { useForm, SubmitHandler } from "react-hook-form";
+
 type Props = {}
 
+
+type Inputs = {
+    name: string,
+    email: string,
+    subject: string,
+    message: string
+  };
+
 function Contact({}: Props) {
-  return (
+    const { 
+        register, handleSubmit 
+    } = useForm<Inputs>();
+    const onSubmit: SubmitHandler<Inputs> = (formdata) => {
+        window.location.href = `mailto:karankartikey72@gmail.com?subject=${formdata.subject}&body=Hi, My name is ${formdata.name}. ${formdata.message} 
+        
+        Regards`
+    };
+
+    return (
+    
+
 
     <div className='flex flex-col relative h-screen max-w-7xl px-10 justify-evenly items-center text-center md:text-left md:flex-row mx-auto '>
         <h3 className='absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl '>
@@ -35,15 +56,15 @@ function Contact({}: Props) {
 
             </div>
 
-            <form className='flex flex-col mx-auto space-y-2 w-fit'>
+            <form onSubmit={handleSubmit(onSubmit)} className='flex flex-col mx-auto space-y-2 w-fit'>
             <div className='flex space-x-2 rounded-full'>
-                <input className='contactInput' type="text" placeholder='Name'/>
-                <input className='contactInput' type="email" placeholder='Email'/>
+                <input {...register('name')} className='contactInput' type="text" placeholder='Name'/>
+                <input {...register('email')} className='contactInput' type="email" placeholder='Email'/>
             </div>
 
-            <input className='contactInput' type="text" placeholder='Subject' />
+            <input {...register('subject')} className='contactInput' type="text" placeholder='Subject' />
 
-            <textarea className='contactInput' placeholder='Message'/>
+            <textarea {...register('message')} className='contactInput' placeholder='Message'/>
             <button className='bg-[#f7ab0a] rounded-md py-5 px-10 text-black font-bold text-lg'>Submit</button>
         </form>
         </div>
