6f6f095ab6cecff42b8ffcccbcc17b98e8324517
karan kartikeya <karankartikey72@gmail.com>
2022-09-27 20:28:02 +0530

    feat:added contact us form

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -10,14 +10,21 @@ function Contact({}: Props) {
             COntact
         </h3>
 
-        <div className='flex flex-col space-y-10'>
+            
+            <div className='flex flex-col space-y-10'>
             <h4 className='text-4xl font-semibold text-center '>
                 I have got what you want bro!!!
             </h4>
-
+            <div className='space-y-10'>
             <div className='flex items-center space-x-5 justify-center'>
                 <PhoneIcon className='animate-pulse h-7 w-7 text-yellow-400'/>
                 <p className='text-2xl'>+917409130005</p>
+            </div>
+
+            
+            <div className='flex items-center space-x-5 justify-center'>
+                <EnvelopeIcon className='animate-pulse h-7 w-7 text-yellow-400'/>
+                <p className='text-2xl' ><a href='mailto:karankartikey72@gmail.com'>Karankartikey72@gmail.com</a></p>
             </div>
 
             <div className='flex items-center space-x-5 justify-center'>
@@ -25,11 +32,24 @@ function Contact({}: Props) {
                 <p className='text-2xl'>123 developer</p>
             </div>
 
-            <div className='flex items-center space-x-5 justify-center'>
-                <EnvelopeIcon className='animate-pulse h-7 w-7 text-yellow-400'/>
-                <p className='text-2xl' ><a href='mailto:karankartikey72@gmail.com'>Karankartikey72@gmail.com</a></p>
+
             </div>
+
+            <form className='flex flex-col mx-auto space-y-2 w-fit'>
+            <div className='flex space-x-2 rounded-full'>
+                <input className='contactInput' type="text" placeholder='Name'/>
+                <input className='contactInput' type="email" placeholder='Email'/>
+            </div>
+
+            <input className='contactInput' type="text" placeholder='Subject' />
+
+            <textarea className='contactInput' placeholder='Message'/>
+            <button className='bg-[#f7ab0a] rounded-md py-5 px-10 text-black font-bold text-lg'>Submit</button>
+        </form>
         </div>
+
+        {/* CONTACT ME FORM */}
+        
     </div>
   )
 }
