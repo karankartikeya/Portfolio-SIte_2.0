c83db56a7957b8dc1805b7738d0d6b4a1085edf4
karan kartikeya <karankartikey72@gmail.com>
2022-09-28 13:35:00 +0530

    deployed dataset on sanity

--- sanity/schemas/schema.js
+++ sanity/schemas/schema.js
@@ -5,10 +5,11 @@ import createSchema from 'part:@sanity/base/schema-creator'
 import schemaTypes from 'all:part:@sanity/base/schema-type'
 
 // We import object and document schemas
-import blockContent from './blockContent'
-import category from './category'
-import post from './post'
-import author from './author'
+import experience from './experience'
+import pageInfo from './pageInfo'
+import projects from './projects'
+import social from './social'
+import skill from './skill'
 
 // Then we give our schema to the builder and provide the result to Sanity
 export default createSchema({
@@ -17,13 +18,10 @@ export default createSchema({
   // Then proceed to concatenate our document type
   // to the ones provided by any plugins that are installed
   types: schemaTypes.concat([
-    // The following are document types which will appear
-    // in the studio.
-    post,
-    author,
-    category,
-    // When added to this list, object types can be used as
-    // { type: 'typename' } in other document schemas
-    blockContent,
+    experience,
+    projects,
+    social,
+    skill,
+    pageInfo,
   ]),
 })
