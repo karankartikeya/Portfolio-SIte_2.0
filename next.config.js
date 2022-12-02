c29aff0bac2af3ccd7469d64922b2a1e641bdd9a
karan kartikeya <karankartikey72@gmail.com>
2022-12-02 19:01:07 +0530

    fix: removed port from next config

--- next.config.js
+++ next.config.js
@@ -7,7 +7,6 @@ const nextConfig = {
       {
         protocol: 'https',
         hostname: 'cdn.sanity.io',
-        port: '',
         pathname: '/images/l7emcy8r/production/**',
       },
     ],
